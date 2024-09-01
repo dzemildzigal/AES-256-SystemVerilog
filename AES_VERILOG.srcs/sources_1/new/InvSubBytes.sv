7780389f4528e7e560d266bb06084875397d1d1a
dzemildzigal <dzemil.dzigal@gmail.com>
2024-09-01 21:46:06 +0200

    Addition of InvSubBytes

--- AES_VERILOG.srcs/sources_1/new/InvSubBytes.sv
+++ AES_VERILOG.srcs/sources_1/new/InvSubBytes.sv
@@ -23,7 +23,26 @@
 module InvSubBytes(
     input clk,
     input rst,
-    input [127:0] in,
-    output [127:0] out
+    input [0:127] input_state,
+    output reg [0:127] output_state,
+    output reg valid_data
     );
+integer i;
+//initialize memory
+reg [0:7] inv_s_box [0:255];
+// Functionality
+// All signals used in a procedural block should be declared as type reg  
+always @ (posedge clk) begin 
+    if (rst) begin
+        output_state <= {128{1'b0}};
+        valid_data <= 1'b0;
+    end
+    else begin
+        for (i = 0; i < 16; i++) begin
+            $readmemh("inv_s_box.mem", inv_s_box);
+            output_state[i*8 +:8] <= inv_s_box[input_state[i*8 +:8]];
+        end
+        valid_data <= 1'b1; 
+    end
+end  
 endmodule
