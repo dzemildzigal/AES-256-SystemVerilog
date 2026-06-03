677db3950196e2d20feb395aff7ccb742e325d0c
dzemildzigal <dzemil.dzigal@gmail.com>
2026-06-03 20:29:47 +0200

    add HDMI_AES_TX project

--- pynq/build_bd_hdmi_aes_tx.tcl
+++ pynq/build_bd_hdmi_aes_tx.tcl
@@ -35,7 +35,12 @@ proc resolve_pynq_base_dir {} {
     if {[info exists ::PYNQ_Z2_BASE_DIR] && $::PYNQ_Z2_BASE_DIR ne ""} {
         return [file normalize $::PYNQ_Z2_BASE_DIR]
     }
-    error "PYNQ_Z2_BASE_DIR is required. Point it at the official PYNQ boards/Pynq-Z2/base directory before sourcing this script."
+    # Common Vivado Tcl typo: set ::(PYNQ_Z2_BASE_DIR) ...
+    # That creates variable named "(PYNQ_Z2_BASE_DIR)" in global namespace.
+    if {[info exists ::(PYNQ_Z2_BASE_DIR)] && $::(PYNQ_Z2_BASE_DIR) ne ""} {
+        return [file normalize $::(PYNQ_Z2_BASE_DIR)]
+    }
+    error "PYNQ_Z2_BASE_DIR is required. Use one of: 'set ::PYNQ_Z2_BASE_DIR <path>' or 'set ::env(PYNQ_Z2_BASE_DIR) <path>', then source script again."
 }
 
 proc require_file {path label} {
@@ -49,6 +54,22 @@ proc require_ip {vlnv} {
     if {[llength $defs] == 0} {
         error "Required Vivado IP '$vlnv' was not found in the current catalog."
     }
+}
+
+proc configure_pynq_ip_repo {pynq_base_dir} {
+    set repo_dir [file normalize [file join $pynq_base_dir ".." ".." "ip"]]
+    if {![file isdirectory $repo_dir]} {
+        puts "WARNING: PYNQ IP repo folder not found at $repo_dir"
+        return
+    }
+
+    set current_repos [get_property ip_repo_paths [current_project]]
+    if {[lsearch -exact $current_repos $repo_dir] < 0} {
+        lappend current_repos $repo_dir
+        set_property ip_repo_paths $current_repos [current_project]
+    }
+
+    update_ip_catalog -rebuild -scan_changes
 }
 
 proc remove_existing_bd {bd_name} {
@@ -96,12 +117,17 @@ if {$current_project_name ne "HDMI_AES_TX"} {
 set pynq_base_dir [resolve_pynq_base_dir]
 require_file [file join $pynq_base_dir "build_base_ip.tcl"] "PYNQ-Z2 base build script"
 require_file [file join $pynq_base_dir "base.tcl"] "PYNQ-Z2 base overlay Tcl"
+configure_pynq_ip_repo $pynq_base_dir
 
 require_ip "xilinx.com:ip:processing_system7:5.5"
 require_ip "xilinx.com:ip:axi_interconnect:2.1"
 require_ip "xilinx.com:ip:v_vid_in_axi4s:5.0"
 require_ip "xilinx.com:ip:v_tc:6.2"
+require_ip "xilinx.com:user:color_swap:1.1"
+require_ip "xilinx.com:ip:xlconstant:1.1"
 require_ip "digilentinc.com:ip:dvi2rgb:1.7"
+require_ip "digilentinc.com:ip:axi_dynclk:1.0"
+require_ip "digilentinc.com:ip:rgb2dvi:1.2"
 
 set pynq_part [detect_pynq_board_part]
 if {$pynq_part ne ""} {
@@ -121,6 +147,46 @@ set_property -dict [list \
 
 create_bd_cell -type module -reference $AES_MODULE $AES_INST
 create_bd_cell -type module -reference $WRITER_MODULE $WRITER_INST
+create_bd_cell -type ip -vlnv digilentinc.com:ip:dvi2rgb:1.7 dvi2rgb_0
+set_property -dict [list \
+    CONFIG.kAddBUFG {false} \
+    CONFIG.kClkRange {1} \
+    CONFIG.kEdidFileName {720p_edid.data} \
+    CONFIG.kRstActiveHigh {false} \
+] [get_bd_cells dvi2rgb_0]
+
+create_bd_cell -type ip -vlnv xilinx.com:user:color_swap:1.1 color_swap_0
+set_property -dict [list \
+    CONFIG.input_format {rbg} \
+    CONFIG.output_format {rgb} \
+] [get_bd_cells color_swap_0]
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:v_vid_in_axi4s:5.0 v_vid_in_axi4s_0
+set_property -dict [list \
+    CONFIG.C_ADDR_WIDTH {12} \
+    CONFIG.C_HAS_ASYNC_CLK {1} \
+] [get_bd_cells v_vid_in_axi4s_0]
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:v_tc:6.2 vtc_in
+set_property -dict [list \
+    CONFIG.HAS_INTC_IF {true} \
+    CONFIG.enable_generation {false} \
+    CONFIG.horizontal_blank_detection {false} \
+    CONFIG.max_lines_per_frame {2048} \
+    CONFIG.vertical_blank_detection {false} \
+] [get_bd_cells vtc_in]
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 hdmi_hpd_const
+set_property -dict [list \
+    CONFIG.CONST_VAL {1} \
+    CONFIG.CONST_WIDTH {1} \
+] [get_bd_cells hdmi_hpd_const]
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 hdmi_vidrst_const
+set_property -dict [list \
+    CONFIG.CONST_VAL {0} \
+    CONFIG.CONST_WIDTH {1} \
+] [get_bd_cells hdmi_vidrst_const]
 
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 \
     -config {make_external "FIXED_IO, DDR" Master "Disable" Slave "Disable"} \
@@ -150,6 +216,18 @@ apply_bd_automation -rule xilinx.com:bd_rule:axi4 \
         master_apm {0}
     } [get_bd_intf_pins $WRITER_INST/S_AXI]
 
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 \
+    -config {
+        Clk_master {/ps7/FCLK_CLK0 (100 MHz)}
+        Clk_slave  {Auto}
+        Clk_xbar   {Auto}
+        Master     {/ps7/M_AXI_GP0}
+        Slave      {/vtc_in/ctrl}
+        ddr_seg    {Auto}
+        intc_ip    {Auto}
+        master_apm {0}
+    } [get_bd_intf_pins vtc_in/ctrl]
+
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 hp0_mem_ic
 set_property -dict [list \
     CONFIG.NUM_MI {1} \
@@ -158,6 +236,36 @@ set_property -dict [list \
 
 connect_bd_intf_net [get_bd_intf_pins $WRITER_INST/M_AXI] [get_bd_intf_pins hp0_mem_ic/S00_AXI]
 connect_bd_intf_net [get_bd_intf_pins hp0_mem_ic/M00_AXI] [get_bd_intf_pins ps7/S_AXI_HP0]
+connect_bd_intf_net [get_bd_intf_pins dvi2rgb_0/RGB] [get_bd_intf_pins color_swap_0/pixel_input]
+connect_bd_intf_net [get_bd_intf_pins color_swap_0/pixel_output] [get_bd_intf_pins v_vid_in_axi4s_0/vid_io_in]
+connect_bd_intf_net [get_bd_intf_pins v_vid_in_axi4s_0/vtiming_out] [get_bd_intf_pins vtc_in/vtiming_in]
+
+if {[llength [get_bd_intf_ports -quiet hdmi_in]] > 0} {
+    connect_bd_intf_net [get_bd_intf_ports hdmi_in] [get_bd_intf_pins dvi2rgb_0/TMDS]
+} else {
+    set hdmi_in_ext [make_bd_intf_pins_external [get_bd_intf_pins dvi2rgb_0/TMDS]]
+    if {[llength $hdmi_in_ext] > 0} {
+        set_property name hdmi_in [lindex $hdmi_in_ext 0]
+    }
+}
+
+if {[llength [get_bd_intf_ports -quiet hdmi_in_ddc]] > 0} {
+    connect_bd_intf_net [get_bd_intf_ports hdmi_in_ddc] [get_bd_intf_pins dvi2rgb_0/DDC]
+} else {
+    set hdmi_in_ddc_ext [make_bd_intf_pins_external [get_bd_intf_pins dvi2rgb_0/DDC]]
+    if {[llength $hdmi_in_ddc_ext] > 0} {
+        set_property name hdmi_in_ddc [lindex $hdmi_in_ddc_ext 0]
+    }
+}
+
+if {[llength [get_bd_intf_ports -quiet hdmi_in_video_out]] > 0} {
+    connect_bd_intf_net [get_bd_intf_ports hdmi_in_video_out] [get_bd_intf_pins v_vid_in_axi4s_0/video_out]
+} else {
+    set hdmi_in_video_out_ext [make_bd_intf_pins_external [get_bd_intf_pins v_vid_in_axi4s_0/video_out]]
+    if {[llength $hdmi_in_video_out_ext] > 0} {
+        set_property name hdmi_in_video_out [lindex $hdmi_in_video_out_ext 0]
+    }
+}
 
 set pkt_pt_axis_in [create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:axis_rtl:1.0 pkt_pt_axis_in]
 set_property -dict [list \
@@ -175,6 +283,18 @@ connect_bd_intf_net [get_bd_intf_pins $AES_INST/M_AXIS_CT] [get_bd_intf_pins $WR
 
 create_bd_port -dir O frame_writer_irq
 connect_bd_net [get_bd_ports frame_writer_irq] [get_bd_pins $WRITER_INST/irq]
+if {[llength [get_bd_ports -quiet hdmi_in_hpd]] > 0} {
+    connect_bd_net [get_bd_ports hdmi_in_hpd] [get_bd_pins hdmi_hpd_const/dout]
+} else {
+    create_bd_port -dir O hdmi_in_hpd
+    connect_bd_net [get_bd_ports hdmi_in_hpd] [get_bd_pins hdmi_hpd_const/dout]
+}
+if {[llength [get_bd_ports -quiet hdmi_vtc_irq]] > 0} {
+    connect_bd_net [get_bd_ports hdmi_vtc_irq] [get_bd_pins vtc_in/irq]
+} else {
+    create_bd_port -dir O hdmi_vtc_irq
+    connect_bd_net [get_bd_ports hdmi_vtc_irq] [get_bd_pins vtc_in/irq]
+}
 
 set ps_fclk0_pin       [get_bd_pins ps7/FCLK_CLK0]
 set ps_fclk_resetn_pin [get_bd_pins ps7/FCLK_RESET0_N]
@@ -188,6 +308,8 @@ foreach p [list \
     [get_bd_pins hp0_mem_ic/M00_ACLK] \
     [get_bd_pins $AES_INST/S_AXI_ACLK] \
     [get_bd_pins $WRITER_INST/S_AXI_ACLK] \
+    [get_bd_pins vtc_in/s_axi_aclk] \
+    [get_bd_pins v_vid_in_axi4s_0/aclk] \
 ] {
     connect_if_unconnected $ps_fclk0_pin $p
 }
@@ -198,9 +320,18 @@ foreach p [list \
     [get_bd_pins hp0_mem_ic/M00_ARESETN] \
     [get_bd_pins $AES_INST/S_AXI_ARESETN] \
     [get_bd_pins $WRITER_INST/S_AXI_ARESETN] \
+    [get_bd_pins vtc_in/s_axi_aresetn] \
+    [get_bd_pins dvi2rgb_0/aRst_n] \
+    [get_bd_pins v_vid_in_axi4s_0/aresetn] \
+    [get_bd_pins vtc_in/resetn] \
 ] {
     connect_if_unconnected $ps_fclk_resetn_pin $p
 }
+
+connect_if_unconnected $ps_fclk0_pin [get_bd_pins dvi2rgb_0/RefClk]
+connect_bd_net [get_bd_pins dvi2rgb_0/PixelClk] [get_bd_pins v_vid_in_axi4s_0/vid_io_in_clk]
+connect_bd_net [get_bd_pins dvi2rgb_0/PixelClk] [get_bd_pins vtc_in/clk]
+connect_bd_net [get_bd_pins hdmi_vidrst_const/dout] [get_bd_pins v_vid_in_axi4s_0/vid_io_in_reset]
 
 assign_bd_address
 regenerate_bd_layout
@@ -213,7 +344,13 @@ if {[llength $bd_files] > 0} {
 }
 
 make_wrapper -files [get_files ${BD_NAME}.bd] -top
-set wrapper_path [file normalize [glob HDMI_AES_TX.gen/sources_1/bd/${BD_NAME}/hdl/${BD_NAME}_wrapper.v]]
+set project_dir [file normalize [get_property DIRECTORY [current_project]]]
+set wrapper_glob [file join $project_dir "${current_project_name}.gen" "sources_1" "bd" $BD_NAME "hdl" "${BD_NAME}_wrapper.v"]
+set wrapper_matches [glob -nocomplain $wrapper_glob]
+if {[llength $wrapper_matches] == 0} {
+    error "Wrapper file not found at expected path: $wrapper_glob"
+}
+set wrapper_path [file normalize [lindex $wrapper_matches 0]]
 add_files -norecurse $wrapper_path
 set_property top ${BD_NAME}_wrapper [current_fileset]
 update_compile_order -fileset sources_1
@@ -227,6 +364,8 @@ puts "  What is real in this slice:"
 puts "    - PS7 control plane"
 puts "    - AES-GCM stream wrapper"
 puts "    - Existing DDR writer path"
+puts "    - HDMI ingress chain: dvi2rgb -> color_swap -> v_vid_in_axi4s"
+puts "    - External HDMI interfaces: hdmi_in, hdmi_in_ddc, hdmi_in_hpd"
 puts "    - External AXIS insertion point: pkt_pt_axis_in"
 puts ""
 puts "  What remains for the literal HDMI TX path:"
